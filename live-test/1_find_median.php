a3fc23718cd48ee6a6550bdc7a29ae8794676443
Ahmad Huda Salam <ahmadhudasalam9@gmail.com>
2026-05-19 19:39:38 +0700

    live test nomor 1

--- live-test/1_find_median.php
+++ live-test/1_find_median.php
@@ -4,7 +4,7 @@
  * 1.⁠ ⁠Buat fungsi untuk mencari angka 'Median' dari sebuah array yang tidak terurut, tanpa menggunakan fungsi sort bawaan.
  */
 
-function findMedian(array $array): int
+function findMedian(array $array)
 {
     $length = count($array);
 
@@ -13,19 +13,29 @@ function findMedian(array $array): int
             if ($array[$j] > $array[$j + 1]) {
                 $temp = $array[$j];
                 $array[$j] = $array[$j + 1];
-                $array_number[$j + 1] = $temp;
+                $array[$j + 1] = $temp;
             }
         }
     }
 
-    $median_number = 0;
+    if ($length % 2 == 0) {
+        $middle1 = ($length / 2) - 1;
+        $middle2 = ($length / 2);
 
-    // for ($i=0; $i < $length / 2; $i++) {
-    //     $median_number =
-    // }
+        $median = ($array[$middle1] + $array[$middle2]) / 2;
+    } else {
+        $middle = ($length - 1) / 2;
+
+        $median = $array[$middle];
+    }
+
+    return $median;
 }
 
 $array_number = [2, 1, 5, 7, 4, -8, -3, -1]; // [-8, -3, -1, 1, 2, 4, 5, 7];
+$array_number2 = [2, 1, 5, 7, 2, 4, -8, -3, -1]; // [-8, -3, -1, 1, 2, 4, 5, 7];
 
 echo findMedian($array_number);
+echo "\n";
+echo findMedian($array_number2);
 echo "\n";
